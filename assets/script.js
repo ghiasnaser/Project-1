37fb6479baef6fb1545da5767a612733ab79cdcd
Mack <mldixon9750@gmail.com>
2023-04-25 21:25:27 -0700

    added psuedocode for JS

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+// Psuedocode
+//
+// Variables
+// const cityEl
+// const submitBtnEl
+// const favoritesEl
+// const container
+//
+//
+//
+// Functions:
+// Event listener for submit buutton
+// startApp()               Funct that reads city name from input & calls weather and health
+// getWeather()             Funct will call getGeoCode()
+// cityWeather()            Funct will get weather for specific city
+// getHealth()              Funct returns nearby health information
+// getPhoto()               Takes city name as input and returns 1 or more photos of location
+// getEvents()              Takes city name and returns nearby events
+// addFavorites()           Adds favorites as an array to local storage
+// displayItems()           Displays items from local storage
+// clearStorage()           Clears items from local storage
+// 
